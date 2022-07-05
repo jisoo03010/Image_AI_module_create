428d91ec81f71dad0f9f1cec437b8cf0f8850916
김지수 <040310jisoo@naver.com>
2022-07-05 17:55:09 +0900

    Add files via upload

--- image_model/app.py
+++ image_model/app.py
@@ -1,3 +1,4 @@
+from cProfile import label
 from flask import Flask, render_template, jsonify, request
 import io
 import json
@@ -31,13 +32,13 @@ batch_size = 32
 num_workers = 0 
 lr = 0.001
 test_accu = []
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") # GPU 여부에 따라서 사용할 디바이스 종류 자동 지정
 
 model = models.densenet161(pretrained=True)
 model.classifier = nn.Linear(2208, 3) 
-model.load_state_dict(torch.load('model.pt'))
+model.load_state_dict(torch.load('model.pt', map_location=device))
 model.eval()
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") # GPU 여부에 따라서 사용할 디바이스 종류 자동 지정
 imagenet_class_index = json.load(open('./static/imagenet_class_index.json'))
 
 
@@ -45,41 +46,52 @@ data_dir = './static/sample_data/'
 train_dir = os.path.join(data_dir, 'train')
 test_dir = os.path.join(data_dir, 'val')
 
-def transform_image(image_bytes):
+@app.route('/predict', methods=['GET'])
+def predict(image_bytes):
+    classes = {0: 'eum_data', 1: 'ma_data', 2: 'son_data'}
     data_transforms = transforms.Compose([transforms.Resize((230, 230)),
                                         transforms.RandomCrop(224),
                                         transforms.ToTensor()])
-    image = Image.open("./static/image/madongseak.jpg")
+    image = Image.open("./static/image/eun.jpg")  # image input 
     image = data_transforms(image)
     image = image.to(device)
     image = image.reshape(1, 3, 224, 224)
+    print("image=>>>", image) # tensor로 변환함 
+
+    pred = model(image)
     #softmax(1) shape을 다 합쳤을때 1이 안되서 소프트맥스를 사용해서 1을 만들어 준뒤에 
-    # 정확도 구하기 하면 끝 pred.argmax(1)
+    pred = F.softmax(pred)
+    # pred = pred.max(pred.data, 1)
+    for p in pred:
+        print('\n =======정확도=======\n{:.4f}% : eum_data, \n{:.4f}% : ma_data, \n{:.4f}% : son_data'.format(p[0]*100, p[1]*100, p[2]*100))
+        print(pred.argmax(1),"번")
     #max값 구하기 
     # 몇 번째 인덱스에 몇 퍼센트인지 수치상으로 보여주기 
-    pred = model(image)
     return pred
 
-
-
-
-with open("./static/image/madongseak.jpg", 'rb') as f:
+#image input 
+with open("./static/image/eun.jpg", 'rb') as f:
     image_bytes = f.read()
-    print("\n", transform_image(image_bytes=image_bytes))
+    print("\n", predict(image_bytes=image_bytes))
 
-#=============================================================================
+#==서버===========================================================================
+
+
 @app.route('/')
 def hello():
-    return 'Hello World!'
+   return "Hellooo"
 
-@app.route('/main')
+
+@app.route('/main', methods=['GET', 'POST'])
 def main():
-    return render_template("main.html")
+    value = 'fdsadfdsf, python'
+    return render_template("main.html", value = value)
 
-@app.route('/predict', methods=['GET'])
-def predict():
-    return 
-     
+@app.route("/upload", methods=['POST'])
+def upload():
+    print(request.files['file_lo'])
+    return jsonify({"abcdd" : "asdf"})
 
 if __name__ == "__main__":
     app.run()
+ 
